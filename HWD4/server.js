302ebf53beddc18f58db8a8a5c2dad43741b3aa5
maiANKIT <ankitsharmaindiaa@gmail.com>
2026-05-19 13:20:09 +0530

    done

--- HWD4/server.js
+++ HWD4/server.js
@@ -12,6 +12,32 @@ app.get('/', (req, res)=>{
     res.send('Welcome to my hotel... How I can help you?')
 })
 
+app.post('/person', (req, res)=>{
+
+    const data = req.body //assuming the request body contains the person data
+
+    const newPerson = new Person(data) //assuming the request body contains the person data
+
+    // newPerson.name = data.name;
+    // newPerson.age = data.age;
+    // newPerson.mobile = data.mobile;
+    // newPerson.email = data.email;
+    // newPerson.address = data.address; //this is too long if we write
+
+    //save the new person to the database
+    newPerson.save((error, person)=>{
+        if(error){
+            console.log('Error saving person', error);
+            res.status(500).json({error: 'Internal server error'})
+        }
+        else{
+            console.log('data saved successfully');
+            res.status(200).json(savedPerson)
+        }
+    })
+
+})
+
 app.listen(3000, ()=>{
     console.log('server is running on port 3000')
 })
